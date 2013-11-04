a30d52e063f92cfdb705de8d38fa53e486da2b5c
haiyang.huang.ca@gmail.com <haiyang.huang.ca@gmail.com>
2013-11-04 01:33:59 -0500

    finish get skewing detection by histogram

---
+++ src/HistroChart.java
@@ -0,0 +1,92 @@
+package proj;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HistroChart extends JPanel {
+	int[] xdata, ydata;
+	int w, h;
+	int xMax, xMin, yMax, yMin;
+	BufferedImage bi;
+	public HistroChart(BufferedImage bi,int w, int h) {
+		this.bi=bi;
+		
+		this.xdata =SkewingHandler.getXDistribution(this.bi, 150, 2, true); 
+		this.ydata =SkewingHandler.getYDistribution(this.bi, 150, 2, true); 
+		this.w = w;
+		this.h = h;
+		this.xMax = xdata[0];
+		this.xMin = this.xMax;
+		for (int i = 0; i < xdata.length; i++) {
+			if (xdata[i] > this.xMax)
+				this.xMax = xdata[i];
+			if (xdata[i] < this.xMin)
+				this.xMin = xdata[i];
+		}
+		this.yMin=ydata[0]; this.yMax=this.yMin;
+		for (int i = 0; i < ydata.length; i++) {
+			if (ydata[i] > this.yMax)
+				this.yMax = ydata[i];
+			if (ydata[i] < this.yMin)
+				this.yMin = ydata[i];
+		}
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		Dimension d = super.getPreferredSize();
+		d.width = this.w+this.padding;
+		d.height = this.h+this.padding;
+		return d;
+	}
+
+	int padding=250;
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(this.bi, 0, 0, this.w, this.h, null);
+		double interval = this.w /(double) xdata.length;
+		for (int i = 0; i < xdata.length; i++) {
+			g.drawLine((int)(interval * i), (xdata[i] - this.xMin) * this.padding
+					/ (this.xMax - this.xMin) + this.h, (int)(i*interval) , this.h+this.padding-2);
+		}
+		interval = this.h / (double)ydata.length;
+		for (int i = 0; i < ydata.length; i++) {
+			g.drawLine((ydata[i] - this.yMin) * this.padding
+					/ (this.yMax - this.yMin) + this.w, (int)(interval *i), this.w+this.padding-2, (int)(interval * i));
+		}
+	}
+	
+	public static void showHistroDistrib(BufferedImage bi, int w, int h, String title){
+		HistroChart hc=new HistroChart(bi, w, h);
+		JFrame jf = new JFrame(title);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.getContentPane().add(hc);
+		jf.pack();
+		jf.setVisible(true);
+	}
+
+
+
+	static public void main(String[] args) throws IOException {
+		final String pathImage = "/home/bart/Pictures/c.png";
+		BufferedImage ia = ImageIO.read(new File(pathImage));
+		BufferedImage ib=ImgTool.rotate(ia, Math.PI/50);
+		JFrame jf = new JFrame();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.getContentPane().add(new HistroChart(ib, 800, 500));
+		jf.pack();
+		jf.setVisible(true);
+	}
+
+}
--- src/SkewingHandler.java
+++ src/SkewingHandler.java
@@ -1,29 +1,157 @@
 package proj;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SkewingHandler {
+	static int[] averageFilterForArray(int[] a, int w) {
+		int[] b = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			int s = a[i];
+			for (int j = 1; j < w; j++) {
+				int k1 = i - j, k2 = i + j;
+				if (k1 < 0)
+					k1 = k2;
+				if (k2 >= a.length)
+					k2 = k1;
+				s += a[k1] + a[k2];
+			}
+			b[i] = s / (2 * w + 1);
+		}
+		return b;
+	}
 
-	static double getSkewAngle(BufferedImage bi, int threadhold) {
+	public static void showData(int[][] data, int w, int h) {
+		JFrame jf = new JFrame();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.getContentPane().setLayout(new GridLayout(data.length, 1));
+		for (int i = 0; i < data.length; i++)
+			// jf.getContentPane().add(new HistroChart(data[i], w, h));
+			jf.pack();
+		jf.setVisible(true);
+	}
 
-		BufferedImage img = ImgTool.toBin(bi, threadhold, false);
-		int w = bi.getWidth(), h = bi.getHeight();
+	static int[] getYDistribution(BufferedImage bi, int threshold,
+			int widthFilter, boolean inverse) {
+
+		BufferedImage img = ImgTool.toBin(bi, threshold, inverse);
+		int h = bi.getHeight(), w = bi.getWidth();
 		int[] yhist = new int[h];
 		int rgbWhite = Color.white.getRGB();
 		for (int y = 0; y < h; y++) {
 			yhist[y] = 0;
-			
-				for (int x = 0; x < w; x++) {
+			for (int x = 0; x < w; x++) {
 				int rgb = img.getRGB(x, y);
 				if (rgb == rgbWhite)
-					yhist[x]++;
+					yhist[y]++;
 			}
 		}
+
+		/*
+		 * smooth the distribution
+		 */
+		int[] yyhist = averageFilterForArray(yhist, widthFilter);
+		return yyhist;
+	}
+
+	static int[] getXDistribution(BufferedImage bi, int threshold,
+			int widthFilter, boolean inverse) {
+
+		BufferedImage img = ImgTool.toBin(bi, threshold, inverse);
+		int h = bi.getHeight(), w = bi.getWidth();
+		int[] xhist = new int[w];
+		int rgbWhite = Color.white.getRGB();
+		for (int x = 0; x < w; x++) {
+			xhist[x] = 0;
+			for (int y = 0; y < h; y++) {
+				int rgb = img.getRGB(x, y);
+				if (rgb == rgbWhite)
+					xhist[x]++;
+			}
+		}
+
+		/*
+		 * smooth the distribution
+		 */
+		int[] xxhist = averageFilterForArray(xhist, widthFilter);
+		return xxhist;
+	}
+
+	static void showImg(BufferedImage bi, String title) {
+		HistroChart.showHistroDistrib(bi, 800, 500, title);
+	}
+
+	static int getWidthPeakYDistribution(int[] yhist) {
+
+		int[] yyhist = averageFilterForArray(yhist, 3);
+
+		/*
+		 * filter the maximum and minum, take the second maximum and minum for
+		 * the threshold
+		 */
+		int max1 = yyhist[0], max2 = max1, min1 = max1, min2 = max1;
+		for (int i = 0; i < yyhist.length; i++) {
+			if (yyhist[i] > max1) {
+				max2 = max1;
+				max1 = yyhist[i];
+			}
+			if (yyhist[i] < min1) {
+				min2 = min1;
+				min1 = yyhist[i];
+			}
+		}
+
+		int div = (int) ((min2 + max2) / (double) (8));
+
+		int idxStart = -1, idxEnd = -1;
+
+		for (int i = yyhist.length / 40; i < yyhist.length; i++) {
+			int j = yyhist.length - i - 1;
+			if (yyhist[i] > div && idxStart < 0)
+				idxStart = i;
+			if (yyhist[j] > div && idxEnd < 0)
+				idxEnd = j;
+			if (idxStart > 0 && idxEnd > 0)
+				break;
+		}
+
+		// System.out.println(String
+		// .format("start: %d, end: %d, width: %d", idxStart, idxEnd,
+		// idxEnd-idxStart));
+
+		return idxEnd - idxStart;
+	}
+
+	static public double getSkewingAngle(BufferedImage bi) {
 		return 0;
 	}
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws IOException {
+		final String pathImage = "/home/bart/Pictures/c.png";
+		BufferedImage ia = ImageIO.read(new File(pathImage));
+		BufferedImage ic = ImgTool.scale(ia, 800, 800);
+		BufferedImage id = ImgTool.rotate(ic, 0);
+
+		for (int i = -20; i < 20; i += 5) {
+			double a = i * Math.PI / (double) 80;
+			BufferedImage ie = ImgTool.rotate(id, a);
+			int[] yDist = SkewingHandler.getYDistribution(ie, 150, 4, false);
+			int[] xDist = SkewingHandler.getXDistribution(ie, 150, 4, false);
+			int h = SkewingHandler.getWidthPeakYDistribution(yDist);
+			int w = SkewingHandler.getWidthPeakYDistribution(xDist);
+			String title = String.format("Angle: %d, product: %d x %d = %d",
+					(int) (180 * a / Math.PI), w, h, w * h);
+			System.out.println(title);
+			SkewingHandler.showImg(ie, title);
+		}
 
 	}
 
